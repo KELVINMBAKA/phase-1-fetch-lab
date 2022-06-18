123ee2ae3a822d9e3dbb7ff465db258a76c071ec
KELVINMBAKA <loverc832@gmail.com>
2022-06-19 01:49:17 +0300

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+
   
-}
+  function fetchBooks() {
+    return fetch("https://anapioficeandfire.com/api/books")
+   .then((res)=>res.json())
+   .then((data)=>renderBooks(data))
+    // To pass the tests, don't forget to return your fetch!
+    
+  }
+
+
+
+
+  
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
